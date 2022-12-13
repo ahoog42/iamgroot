39de67d9db3a0aefc8a244e5b7a02532aead2feb
Andrew Hoog <ahoog42@gmail.com>
2022-12-13 10:29:58 -0600

    first commit

---
+++ I am Groot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Groot
+//
+//  Created by Hiro Protagonist on 11/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# I am Groot
+This is my first iOS project and it will serve as a playgroud to learn iOS programming and demonstrate mobile app security and privacy testing as well as DevOps automation with GitHub Actions.
